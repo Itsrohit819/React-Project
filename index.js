f380b4f1ba395b40300eeb033d0f6fcd2dede737
Rohit <singhrohitckt819@gmail.com>
2025-08-19 13:44:32 +0530

    JSX start

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ const element = React.createElement(
     "h1", { style: { color: "black" } }, "Welcome Defender087"
 );
 
+const jsxElement = <h1 id="heading">Competitive Programmer</h1>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(element);
+root.render(jsxElement);
